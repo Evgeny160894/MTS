dde88902f86c8a6496b58bfd19af4641f8f81829
jadov <jadov94@gmail.com>
2020-04-08 21:28:46 +0300

    Подключил allure, но не починил путь к папке glue для опредения расположения степов

--- src/test/java/tests/stepsDefinitions/GoogleSteps.java
+++ src/test/java/tests/stepsDefinitions/GoogleSteps.java
@@ -1,5 +1,6 @@
 package tests.stepsDefinitions;
 
+import io.qameta.allure.Step;
 import tests.pages.Google;
 import io.cucumber.java.ru.Тогда;
 
@@ -7,6 +8,7 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class GoogleSteps extends Google {
 
+    @Step("Test")
     @Тогда("Переход на страницу {string}")
     public void goToPage(String link) {
         open(link);
